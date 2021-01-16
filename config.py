d0e3d280e669a58b61c8612181a3b199135fc29a
elinoyf <elinoyf@campus.technion.ac.il>
2021-01-16 13:23:32 +0200

    no message

--- config.py
+++ config.py
@@ -7,80 +7,22 @@ Created on Wed May 27 17:19:37 2020
 import configparser
 config = configparser.ConfigParser()
 
-#paths
-config['paths'] = {'in_data_path' :'./data/movie17/',
-                   'out_data_path_seg' : './data/movie17_seg/',
-                   'out_data_path_of' : './data/movie17_of/',
-                   'out_data_path_tools' : './data/movie17_tools/'}
+#model
+config['model'] = {}
 
-#parameters
-config['parameters'] = {'in_height' : 1080,
-                        'in_width' : 1920,
-                        'padded_width' : 'in_width',
-                        'padded_height' : 1536,
-                        'resize_factor' : 3,
-                        'gpu_str' : "cuda",
-                        'cpu_str' : "cpu",
-                        'semantic_segmentation_factor' : 127,
-                        'random_state' : 42,
-                        'frame_resize_factor' : 2.828}
+#loss
+config['loss'] = {}
 
 
-#thresholds
-config['thresholds'] = {'tool_size_thresh' : 5000,
-                        'tool_geo_dist_thresh' : 100,
-                        'tool_of_dist_thresh' : 1,
-                        'tool_angle_thresh' : 15,
-                        'affinity_thresh' : 1.5,
-                        'tool_max_val' : 1000,
-                        'tool_shape_contour_ratio_thresh' : 1.25,
-                        'tool_reliability_thresh' : 0.75,
-                        'min_tool_area' : 2500,
-                        'curve_sampling_factor' : 5,
-                        'min_n_components' : 1,
-                        'max_split_tools' : 4,
-                        'min_prob' : 0.98,
-                        'min_tool_shape' : 50,
-                        'max_n_tools' : 5,
-                        'min_tool_size' : 1200,
-                        'num_of_init_frames' : 3,
-                        'num_out_of_frame' : 3,
-                        'max_tip_distance' : 150,
-                        'end_init_mode' : 3,
-                        'remove_from_list' : 3,
-                        'split_based_on_tracker' : 0.1,
-                        'connect_based_on_tracker' : 0.1,
-                        'min_tip_dist' : 50}
+#loader
+config['loader'] = {'path':'new data', 'batch size':1, 'val percent':0.1}
 
 
-#split thresholds
-config['split_thresholds'] = {'split_contour_points_thresh': 10,
-                              'split_contour_convexity_defect_dist': 10000,
-                              'split_contour_leftover_dist': -20,
-                              'split_contour_leftover_shape': 10}
+#optimizer
+config['optimizer'] = {}
 
-#update tools list thresholds
-config['update_tools_list_thresholds'] = {'new_tool_reliability_thresh': 0.8,
-                                          'update_from_seg_reliability': 0.8,
-                                          'update_from_seg_dice': 0.9,
-
-                                          'update_from_tracker_dice': 0.8,
-                                          'num_of_frames_delay': 2,
-                                          'reduction_level': 0.05,
-                                          'tip_reliability_thresh': 0.95,
-                                          'segmentation_filter_thresh': 0.9,
-                                          'update_tip_loc_old_thresh': 0.9,
-                                          'update_tip_loc_new_thresh': 0.1,
-                                          'min_tip_reliability':0.2}
-
-
-# models
-config['models'] = {'segmentation_model_path' : 'G:/My Drive/TRX_DL_DataBase/Model/Binary_and_semantic_network/model_0_ep_40.pt'}
-
-
-#global params
-config ['backwarp_tenGrid'] = {}
-
+#train
+config['train'] = {}
 
 
 with open('cfg_file.ini', 'w') as configfile:
--- main.py
+++ main.py
@@ -6,8 +6,30 @@ from utils import get_logger
 import torch.optim as optim
 from utils import get_number_of_learnable_parameters
 from trainer import UNet3DTrainer
+from torch.utils.data import DataLoader, random_split
+from BasicDataSet import BasicDataset
 import configparser
+import pathlib
 
+
+def _LoadData(config):
+    cwd = pathlib.Path.cwd()
+    last_dir = cwd.name
+    cwd = str(cwd)
+    print(cwd)
+    last_dir = str(last_dir)
+    imgs_dir = cwd.replace(last_dir, config['loader']['path'])
+    print(imgs_dir)
+    dataset = BasicDataset(imgs_dir)
+    val_percent=config['loader']['val percent']
+    batch_size=config['loader']['batch size']
+    n_val = int(len(dataset) * val_percent)
+    n_train = len(dataset) - n_val
+    train, val = random_split(dataset, [n_train, n_val])
+    train_loader = DataLoader(train, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
+    val_loader = DataLoader(val, batch_size=batch_size, shuffle=False, num_workers=8, pin_memory=True, drop_last=True)
+    loader_dict={'train': train_loader ,'val':val_loader }
+    return loader_dict
 
 def _create_trainer(config, model, optimizer, loss_criterion, eval_criterion, loaders):
     assert 'trainer' in config, 'Could not find trainer configuration'
@@ -40,51 +62,52 @@ def main():
     # Load and log experiment configuration
     config = configparser.ConfigParser()
     config.sections()
-    config.read(r"C:\Users\User\Documents\FinalProject\FinalProject-BraTS\cfg_file.ini")
+    config.read(r"C:\Users\Elinoy\Documents\project\final project\cfg_file.ini")
     print(config['paths']['in_data_path'])
     logger = get_logger('UNet3DTrain')
     logger.info(config)
-
-    manual_seed = config.get('manual_seed', None)
-    if manual_seed is not None:
-        logger.info(f'Seed the RNG for all devices with {manual_seed}')
-        torch.manual_seed(manual_seed)
-        # see https://pytorch.org/docs/stable/notes/randomness.html
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
-
-    # Create the model
-    model = get_model(config)
-    # use DataParallel if more than 1 GPU available
-    device = config['device']
-    if torch.cuda.device_count() > 1 and not device.type == 'cpu':
-        model = nn.DataParallel(model)
-        logger.info(f'Using {torch.cuda.device_count()} GPUs for training')
-
-    # put the model on GPUs
-    logger.info(f"Sending the model to '{config['device']}'")
-    model = model.to(device)
-
-    # Log the number of learnable parameters
-    logger.info(f'Number of learnable params {get_number_of_learnable_parameters(model)}')
-
-    # Create loss criterion
-    loss_criterion = get_loss_criterion(config)
-    # Create evaluation metric
-    eval_criterion = get_evaluation_metric(config)
-
-    # Create data loaders
-    loaders = get_train_loaders(config)
-
-    # Create the optimizer
-    optimizer = _create_optimizer(config, model)
-
-    # Create model trainer
-    trainer = _create_trainer(config, model=model, optimizer=optimizer,
-                              loss_criterion=loss_criterion, eval_criterion=eval_criterion, loaders=loaders)
-    # Start training
-    trainer.fit()
-
-
-if __name__ == '__main__':
-    main()
+    LoadData = _LoadData(config)
+#
+#     manual_seed = config.get('manual_seed', None)
+#     if manual_seed is not None:
+#         logger.info(f'Seed the RNG for all devices with {manual_seed}')
+#         torch.manual_seed(manual_seed)
+#         # see https://pytorch.org/docs/stable/notes/randomness.html
+#         torch.backends.cudnn.deterministic = True
+#         torch.backends.cudnn.benchmark = False
+#
+#     # Create the model
+#     model = get_model(config)
+#     # use DataParallel if more than 1 GPU available
+#     device = config['device']
+#     if torch.cuda.device_count() > 1 and not device.type == 'cpu':
+#         model = nn.DataParallel(model)
+#         logger.info(f'Using {torch.cuda.device_count()} GPUs for training')
+#
+#     # put the model on GPUs
+#     logger.info(f"Sending the model to '{config['device']}'")
+#     model = model.to(device)
+#
+#     # Log the number of learnable parameters
+#     logger.info(f'Number of learnable params {get_number_of_learnable_parameters(model)}')
+#
+#     # Create loss criterion
+#     loss_criterion = get_loss_criterion(config)
+#     # Create evaluation metric
+#     eval_criterion = get_evaluation_metric(config)
+#
+#     # Create data loaders
+#     loaders = get_train_loaders(config)
+#
+#     # Create the optimizer
+#     optimizer = _create_optimizer(config, model)
+#
+#     # Create model trainer
+#     trainer = _create_trainer(config, model=model, optimizer=optimizer,
+#                               loss_criterion=loss_criterion, eval_criterion=eval_criterion, loaders=loaders)
+#     # Start training
+#     trainer.fit()
+#
+#
+# if __name__ == '__main__':
+#     main()
