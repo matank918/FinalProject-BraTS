added BCEDiceLoss and fixed the normalization process

Data Exploration/DataExplorer.py
@@ -25,9 +25,12 @@ def show_image(image, name):
 
 def histogram_image(image):
     # print("unique value:", np.unique(image))
-    print("mean:", torch.mean(image))
-    print("var:", torch.var(image))
-    print("std:", torch.std(image))
+    # print("mean:", torch.mean(image))
+    # print("var:", torch.var(image))
+    # print("std:", torch.std(image))
+    print("mean for non-zero voxels:", torch.mean(image[image.nonzero(as_tuple=True)]))
+    print("var for non zero voxels:", torch.var(image[image.nonzero(as_tuple=True)]))
+    print("std for non zero voxels:", torch.std(image[image.nonzero(as_tuple=True)]))
 
     plt.figure('historgram')
     result = image.flatten()
@@ -45,8 +48,8 @@ if __name__ == '__main__':
     rand_crop = RandomCrop3D(data.shape, net_dim)
     one_hot = OneHotEncoding3d(label.shape)
     # load data
-    # data, label = rand_crop(normalize(to_tensor(data)), to_tensor(one_hot(label)))
-    data, label = normalize(to_tensor(data)), to_tensor(one_hot(label))
+    # data, label = rand_crop(to_tensor(data), to_tensor(one_hot(label)))
+    data, label = rand_crop(normalize(to_tensor(data)), to_tensor(one_hot(label)))
 
     # display data
     mri1, mri2, mri3, mri4 = split_channels(data, dim=0)
@@ -62,5 +65,5 @@ if __name__ == '__main__':
 
     # Sagittal_ch1, Coronal_ch1, Horizontal_ch1 = split_image(ch1)
 
-    # histogram_image(Sagittal_ch1)
+    histogram_image(mri1)
 

DataLoader/CustomTransformations.py
@@ -71,8 +71,11 @@ class RandomCrop3D(object):
 class CustomNormalize(object):
 
     def __call__(self, x):
-        for ch in range(x.shape[0]):
-            x[ch] = (x[ch] - torch.mean(x[ch])) / torch.std(x[ch])
+        for i in range(x.shape[0]):
+            non_zero_vox = x[i][x[i].nonzero(as_tuple=True)]
+            x[i][x[i].nonzero(as_tuple=True)] = \
+                (x[i][x[i].nonzero(as_tuple=True)]-torch.mean(non_zero_vox))/torch.std(non_zero_vox)
+            # x[ch] = (x[ch] - torch.mean(x[ch])) / torch.std(x[ch])
 
         return x
 

Loss/loss.py
@@ -5,15 +5,9 @@ from torch.autograd import Variable
 from utils import flatten
 import math
 
-class _AbstractDiceLoss(nn.Module):
-    """Base class for different implementations of Dice loss."""
-
+class DiceLoss(nn.Module):
     def __init__(self):
-        super(_AbstractDiceLoss, self).__init__()
-
-    def dice(self, input, target):
-        # actual Dice score computation; to be implemented by the subclass
-        raise NotImplementedError
+        super().__init__()
 
     def forward(self, input, target):
         # compute per channel Dice coefficient
@@ -21,11 +15,6 @@ class _AbstractDiceLoss(nn.Module):
 
         # average Dice score across all channels/classes
         return 1 - torch.mean(per_channel_dice)
-
-
-class DiceLoss(_AbstractDiceLoss):
-    def __init__(self):
-        super().__init__()
 
     def dice(self, input, target):
         return self.compute_per_channel_dice(input, target)
@@ -50,40 +39,30 @@ class DiceLoss(_AbstractDiceLoss):
 
         # compute per channel Dice Coefficient
         intersect = 2 * (input * target).sum(-1)
-        denominator = input.sum(-1) + target.sum(-1)
-        # denominator = torch.square(input).sum(-1) + torch.square(target).sum(-1)
+        # denominator = input.sum(-1) + target.sum(-1)
+        denominator = torch.square(input).sum(-1) + torch.square(target).sum(-1)
         result = (intersect + epsilon) / (denominator + epsilon)
         return result
 
+class BCEDiceLoss(nn.Module):
+    """Linear combination of BCE and Dice losses"""
 
-class CustomBCELoss(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.loss = nn.BCELoss()
+    def __init__(self, alpha=1.0, beta=1.0):
+        super(BCEDiceLoss, self).__init__()
+        self.alpha = alpha
+        self.bce = nn.BCELoss()
+        self.beta = beta
+        self.dice = DiceLoss()
 
     def forward(self, input, target):
-        assert input.size() == target.size(), "'input' and 'target' must have the same shape"
-
-        input = flatten(input)
-        target = flatten(target)
-        loss = torch.zeros(input.shape[0], dtype=torch.float32)
-        for i in range(loss.shape[0]):
-            ones = torch.count_nonzero(target[i]).item()
-            total = list(target[i].shape)[0]
-            if ones != 0:
-                weight = (total/ones)
-            else:
-                weight = 1000
-            loss[i] = weight*self.loss(input[i],target[i])
-
-        return torch.mean(loss)
+        return self.alpha * self.bce(input, target) + self.beta * self.dice(input, target)
 
 
 def create_loss(name):
     if name == 'DiceLoss':
         return DiceLoss()
-    elif name == "BCELoss":
-        return CustomBCELoss()
+    elif name == "BCEDiceLoss":
+        return BCEDiceLoss()
 
 
 if __name__ == '__main__':
@@ -91,6 +70,6 @@ if __name__ == '__main__':
 
     rand_image1 = torch.empty(1, 4, 128, 128, 128, dtype=torch.float32).random_(2)
     rand_image2 = torch.empty(1, 4, 128, 128, 128, dtype=torch.float32).random_(2)
-    loss = CustomBCELoss()
-    t = loss(rand_image1, rand_image2)
+    loss = BCEDiceLoss(0.5,0.5)
+    t = loss(rand_image1, rand_image1)
     print(t)

main.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, random_split
+from torch_poly_lr_decay import PolynomialLRDecay
 
 from utils import get_number_of_learnable_parameters, get_logger
 from trainer import UNet3DTrainer
@@ -58,7 +59,8 @@ def _create_optimizer(model):
 
 
 def _create_lr_scheduler(optimizer):
-    return optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30, 80], gamma=0.1)
+    return PolynomialLRDecay(optimizer, max_decay_steps=cfg.max_decay_steps,
+                             end_learning_rate=cfg.end_learning_rate, power=cfg.power)
 
 
 if __name__ == '__main__':
@@ -96,7 +98,7 @@ if __name__ == '__main__':
     optimizer = _create_optimizer(model)
 
     # Create learning rate adjustment strategy
-    lr_scheduler = None
+    lr_scheduler = _create_lr_scheduler(optimizer)
 
     # Create model trainer
     trainer = _create_trainer(model=model, optimizer=optimizer, device=device, logger=logger,